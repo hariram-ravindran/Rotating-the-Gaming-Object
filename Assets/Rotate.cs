9551d5f77549d4836184f5275a8adcf75a86513b
Hariram <hram62625@gmail.com>
2025-08-19 17:57:17 +0530

    addes

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -13,6 +11,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // type the code here
+        transform.RotateAround(Vector3.left, Vector3.up, 40*Time.deltaTime);
     }
 }
